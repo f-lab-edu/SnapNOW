77e10a12be0b988c8fa14613a8a9c1a2f76feeed
김혜주 <sueelly@naver.com>
2024-11-13 20:48:57 +0900

    init: Initialize SnapNOW project

---
+++ src/main/java/kr/flab/snapnow/SnapnowApplication.java
@@ -0,0 +1,13 @@
+package kr.flab.snapnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapnowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnapnowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/flab/snapnow/SnapnowApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.flab.snapnow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnapnowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
