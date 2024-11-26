7b7552e19f94843bc6c2fd62f92a81ffb8cbcba9
김혜주 <sueelly@naver.com>
2024-11-27 00:56:36 +0900

    feat(UserEntity): Remove BaseEntity from UserAccount/Profile/SettingEntity
    UserEntity extends BaseEntity and others don't need BaseEntity.
    Related to #8

--- snapnow-domain/src/main/java/kr/flab/snapnow/domain/user/entity/UserProfileEntity.java
+++ snapnow-domain/src/main/java/kr/flab/snapnow/domain/user/entity/UserProfileEntity.java
@@ -1,55 +1,19 @@
 package kr.flab.snapnow.domain.user.entity;
 
-import java.time.LocalDateTime;
-
 import lombok.Getter;
-import lombok.experimental.SuperBuilder;
-
-import kr.flab.snapnow.core.entity.BaseEntity;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Getter
-@SuperBuilder
-public class UserProfileEntity extends BaseEntity {
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserProfileEntity {
 
     private Long id;
     private String userName;
     private String fullName;
     private String biography;
     private String profileImageUrl;
-
-    public UserProfileEntity(Long id, String userName, String fullName, String biography, String profileImageUrl) {
-        super();
-
-        this.id = id;
-        this.userName = userName;
-        this.fullName = fullName;
-        this.biography = biography;
-        this.profileImageUrl = profileImageUrl;
-    }
-
-    public UserProfileEntity(Long id, String userName, String fullName, String biography, String profileImageUrl, LocalDateTime createdAt, LocalDateTime updatedAt, LocalDateTime deletedAt) {
-        super(createdAt, updatedAt, deletedAt);
-
-        this.id = id;
-        this.userName = userName;
-        this.fullName = fullName;
-        this.biography = biography;
-        this.profileImageUrl = profileImageUrl;
-    }
-
-    public void userNameUpdate(String userName) {
-        this.userName = userName;
-    }
-
-    public void fullNameUpdate(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public void biographyUpdate(String biography) {
-        this.biography = biography;
-    }
-
-    public void profileImageUrlUpdate(String profileImageUrl) {
-        this.profileImageUrl = profileImageUrl;
-    }
 }
--- snapnow-domain/src/main/java/kr/flab/snapnow/domain/user/entity/UserSettingEntity.java
+++ snapnow-domain/src/main/java/kr/flab/snapnow/domain/user/entity/UserSettingEntity.java
@@ -1,47 +1,24 @@
 package kr.flab.snapnow.domain.user.entity;
 
-import java.time.LocalDateTime;
 import java.util.Map;
 
 import lombok.Getter;
-import lombok.experimental.SuperBuilder;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
-import kr.flab.snapnow.core.entity.BaseEntity;
 import kr.flab.snapnow.domain.user.enums.setting.setting_option.NotificationSettingOption;
 import kr.flab.snapnow.domain.user.enums.setting.setting_option.PrivacySettingOption;
 import kr.flab.snapnow.domain.user.enums.setting.setting_type.NotificationSetting;
 import kr.flab.snapnow.domain.user.enums.setting.setting_type.PrivacySetting;
 
 @Getter
-@SuperBuilder
-public class UserSettingEntity extends BaseEntity {
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserSettingEntity {
 
     private Long userId;
     private Map<NotificationSetting, NotificationSettingOption> notificationSettings;
     private Map<PrivacySetting, PrivacySettingOption> privacySettings;
-
-    public UserSettingEntity(
-        Long userId,
-        Map<NotificationSetting, NotificationSettingOption> notificationSettings,
-        Map<PrivacySetting, PrivacySettingOption> privacySettings) {
-        super();
-
-        this.userId = userId;
-        this.notificationSettings = notificationSettings;
-        this.privacySettings = privacySettings;
-    }
-
-    public UserSettingEntity(
-        Long userId,
-        Map<NotificationSetting, NotificationSettingOption> notificationSettings,
-        Map<PrivacySetting, PrivacySettingOption> privacySettings,
-        LocalDateTime createdAt,
-        LocalDateTime updatedAt,
-        LocalDateTime deletedAt) {
-        super(createdAt, updatedAt, deletedAt);
-
-        this.userId = userId;
-        this.notificationSettings = notificationSettings;
-        this.privacySettings = privacySettings;
-    }
 }
