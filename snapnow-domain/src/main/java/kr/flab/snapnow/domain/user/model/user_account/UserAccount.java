04c1eb07dfc53d209a9b2dd2de50e93b9d037ca0
김혜주 <sueelly@naver.com>
2024-11-27 01:55:34 +0900

    feat(UserAccount): Add simple validations in UserAccount domain model
    Related to #8

--- snapnow-domain/src/main/java/kr/flab/snapnow/domain/user/model/user_account/UserAccount.java
+++ snapnow-domain/src/main/java/kr/flab/snapnow/domain/user/model/user_account/UserAccount.java
@@ -2,24 +2,29 @@ package kr.flab.snapnow.domain.user.model.user_account;
 
 import java.time.LocalDate;
 import java.util.Locale;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import lombok.Getter;
 import lombok.Builder;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
 
 import kr.flab.snapnow.domain.user.enums.AuthProvider;
 import kr.flab.snapnow.domain.user.enums.Gender;
 
 @Getter
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class UserAccount {
 
     private Long id;
+
+    @Email
     private String email;
+
+    @NotBlank(message = "Password cannot be blank")
     private String password;
+
+    @NotNull(message = "Auth provider cannot be null")
     private AuthProvider authProvider;
     private String providerId;
 
@@ -35,4 +40,14 @@ public class UserAccount {
 
     @Builder.Default
     private boolean verifiedEmail = false;
+
+    public static class UserAccountBuilder {
+
+        public UserAccountBuilder build() {
+            if (this.email == null && this.providerId == null) {
+                throw new IllegalArgumentException("Email or providerId must be provided");
+            }
+            return this;
+        }
+    }
 }
