43c8c2606a404a39871fdccedfa3adf6de67055a
김혜주 <sueelly@naver.com>
2024-11-27 13:04:20 +0900

    feat(UserSettingDomain): add superBuilder

--- snapnow-domain/src/main/java/kr/flab/snapnow/domain/user/model/user_setting/UserSetting.java
+++ snapnow-domain/src/main/java/kr/flab/snapnow/domain/user/model/user_setting/UserSetting.java
@@ -1,15 +1,18 @@
 package kr.flab.snapnow.domain.user.model.user_setting;
 
 import java.util.Set;
-import java.util.HashSet;
 
 import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+import lombok.NoArgsConstructor;
 
 import kr.flab.snapnow.domain.user.enums.setting.SettingCategory;
 import kr.flab.snapnow.domain.user.enums.setting.setting_option.SettingOption;
 import kr.flab.snapnow.domain.user.enums.setting.setting_type.SettingType;
 
 @Getter
+@SuperBuilder
+@NoArgsConstructor
 public abstract class UserSetting<
     T extends Enum<T> & SettingType, U extends Enum<U> & SettingOption> {
 
