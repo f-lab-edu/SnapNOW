dcef6491eda421aba9ca3788b6d76597f8ab87aa
김혜주 <sueelly@naver.com>
2024-11-27 22:13:14 +0900

    feat(UserDomain): Add UserException types
    Related to #8

--- snapnow-domain/src/main/java/kr/flab/snapnow/domain/user/domain/enums/UserExceptionType.java
+++ snapnow-domain/src/main/java/kr/flab/snapnow/domain/user/domain/enums/UserExceptionType.java
@@ -9,12 +9,21 @@ import kr.flab.snapnow.core.enums.ResultCode;
 @RequiredArgsConstructor
 public enum UserExceptionType implements ResultCode {
 
+    USER_NOT_FOUND("User not found"),
+    DELETED_USER("Deleted user"),
+    USER_ALREADY_EXISTS("User already exists"),
+
     // UserAccount
-    IDENTITY_MUST_BE_PROVIDED("Email or providerId must be provided"),
+    IDENTITY_REQUIRED("Email or providerId must be provided"),
     INVALID_EMAIL("Invalid email"),
-    PASSWORD_CANNOT_BE_BLANK("Password cannot be blank"),
-    AUTH_PROVIDER_CANNOT_BE_NULL("Auth provider cannot be null"),
     INVALID_PASSWORD("Invalid password"),
+    AUTH_PROVIDER_CANNOT_BE_NULL("Auth provider cannot be null"),
+    ACCOUNT_NOT_FOUND("Account not found"),
+    ACCOUNT_ALREADY_EXISTS("Account already exists"),
+
+    // UserProfile
+    USER_PROFILE_NOT_FOUND("User profile not found"),
+    USER_NAME_ALREADY_EXISTS("User name already exists"),
 
     // UserSetting
     INVALID_SETTING("Invalid matching setting category, type or option");
