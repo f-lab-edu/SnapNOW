7cfaffc1bda174a20c4ccd866784542505f3c0d2
김혜주 <sueelly@naver.com>
2024-11-27 23:36:57 +0900

    feat(UserProfileDomain): Add validation method to UserProfileDomain using all args constructor
    Can't override @SuperBuilder's build method, so added to constructor
    Related to #8

--- snapnow-domain/src/main/java/kr/flab/snapnow/domain/user/domain/model/userProfile/CompactProfileWithFollowStatus.java
+++ snapnow-domain/src/main/java/kr/flab/snapnow/domain/user/domain/model/userProfile/CompactProfileWithFollowStatus.java
@@ -4,10 +4,25 @@ import kr.flab.snapnow.domain.user.domain.enums.FollowStatus;
 
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import kr.flab.snapnow.domain.user.domain.exception.UserRequiredArgumentException;
 
 @Getter
 @SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class CompactProfileWithFollowStatus extends CompactProfile {
 
     private FollowStatus followStatus;
+
+    private CompactProfileWithFollowStatus(
+            Long userId, String userName, String profileImageUrl, FollowStatus followStatus) {
+        super(userId, userName, profileImageUrl);
+
+        if (followStatus == null) {
+            throw new UserRequiredArgumentException("Follow status is required");
+        }
+        this.followStatus = followStatus;
+    }
 }
