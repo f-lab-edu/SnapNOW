5633a5d218c19afb9f289361a9b129241533c32f
김혜주 <sueelly@naver.com>
2024-11-27 23:50:34 +0900

    feat(UserSettingDomain): Add Builder and contstuctor with private access level
    Related to #8

--- snapnow-domain/src/main/java/kr/flab/snapnow/domain/user/domain/model/userSetting/NotificationSetting.java
+++ snapnow-domain/src/main/java/kr/flab/snapnow/domain/user/domain/model/userSetting/NotificationSetting.java
@@ -1,13 +1,25 @@
 package kr.flab.snapnow.domain.user.domain.model.userSetting;
 
+import java.util.Map;
 import java.util.HashMap;
+
+import lombok.Getter;
+import lombok.Builder;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 
 import kr.flab.snapnow.domain.user.domain.enums.setting.SettingCategory;
 import kr.flab.snapnow.domain.user.domain.enums.setting.settingOption.NotificationSettingOption;
 import kr.flab.snapnow.domain.user.domain.enums.setting.settingType.NotificationSettingType;
 import kr.flab.snapnow.domain.user.domain.exception.InvalidSettingException;
 
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class NotificationSetting extends UserSetting<NotificationSettingType, NotificationSettingOption> {
+
+    private SettingCategory category;
+    private Map<NotificationSettingType, NotificationSettingOption> settings;
 
     public NotificationSetting() {
         this.category = SettingCategory.NOTIFICATION;
@@ -20,11 +32,11 @@ public class NotificationSetting extends UserSetting<NotificationSettingType, No
 
     public static class NotificationSettingBuilder {
 
-        public static NotificationSetting category(SettingCategory category) {
+        public NotificationSetting build() {
             if (!SettingCategory.NOTIFICATION.equals(category)) {
                 throw new InvalidSettingException();
             }
-            return new NotificationSetting();
+            return new NotificationSetting(category, settings);
         }
     }
 }
--- snapnow-domain/src/main/java/kr/flab/snapnow/domain/user/domain/model/userSetting/UserSetting.java
+++ snapnow-domain/src/main/java/kr/flab/snapnow/domain/user/domain/model/userSetting/UserSetting.java
@@ -2,17 +2,10 @@ package kr.flab.snapnow.domain.user.domain.model.userSetting;
 
 import java.util.Map;
 
-import lombok.Getter;
-import lombok.experimental.SuperBuilder;
-import lombok.NoArgsConstructor;
-
 import kr.flab.snapnow.domain.user.domain.enums.setting.SettingCategory;
 import kr.flab.snapnow.domain.user.domain.enums.setting.settingOption.SettingOption;
 import kr.flab.snapnow.domain.user.domain.enums.setting.settingType.SettingType;
 
-@Getter
-@SuperBuilder
-@NoArgsConstructor
 public abstract class UserSetting<
     T extends Enum<T> & SettingType, U extends Enum<U> & SettingOption> {
 
