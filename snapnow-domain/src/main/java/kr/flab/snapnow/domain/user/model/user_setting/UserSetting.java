c40b566e745bc9111586df7790d4b96d2b47198e
김혜주 <sueelly@naver.com>
2024-11-27 11:04:40 +0900

    feat(UserSettingDomain): Create UserSetting abstract class
    Setting set corresponding to a category
    
    Related to #8

---
+++ snapnow-domain/src/main/java/kr/flab/snapnow/domain/user/model/user_setting/UserSetting.java
@@ -0,0 +1,39 @@
+package kr.flab.snapnow.domain.user.model.user_setting;
+
+import java.util.Set;
+import java.util.HashSet;
+
+import lombok.Getter;
+
+import kr.flab.snapnow.domain.user.enums.setting.SettingCategory;
+import kr.flab.snapnow.domain.user.enums.setting.setting_option.SettingOption;
+import kr.flab.snapnow.domain.user.enums.setting.setting_type.SettingType;
+
+@Getter
+public abstract class UserSetting<
+    T extends Enum<T> & SettingType, U extends Enum<U> & SettingOption> {
+
+    protected SettingCategory category;
+    protected Set<UserSettingElement<T, U>> settings;
+
+    public UserSetting(SettingCategory category) {
+        this.category = category;
+        this.settings = new HashSet<>();
+
+        this.category.getSettingType().forEach(settingType -> {
+            this.settings.add((UserSettingElement<T, U>) new UserSettingElement<>(settingType));
+        });
+    }
+
+    public void updateSetting(UserSettingElement<T, U> setting) {
+        this.settings.stream()
+            .filter(s -> s.getSettingType().equals(setting.getSettingType()))
+            .findFirst()
+            .ifPresent(s -> s.updateSettingOption(setting.getSettingOption()));
+    }
+
+    public void updateSetting(T settingType, U settingOption) {
+        this.settings.stream().filter(setting -> setting.getSettingType().equals(settingType))
+                .findFirst().ifPresent(setting -> setting.updateSettingOption(settingOption));
+    }
+}
