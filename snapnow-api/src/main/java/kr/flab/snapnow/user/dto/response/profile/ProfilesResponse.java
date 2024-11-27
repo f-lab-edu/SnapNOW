a861cba80899ce538d3137013cedac82bba26a0b
김혜주 <sueelly@naver.com>
2024-11-27 18:17:16 +0900

    feat(UserProfileDto): Remove constructor and add builder to response dto

--- snapnow-api/src/main/java/kr/flab/snapnow/user/dto/response/profile/ProfilesResponse.java
+++ snapnow-api/src/main/java/kr/flab/snapnow/user/dto/response/profile/ProfilesResponse.java
@@ -3,12 +3,10 @@ package kr.flab.snapnow.user.dto.response.profile;
 import java.util.List;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
+@Builder
 public class ProfilesResponse {
 
     private List<ProfileElement> profiles;
