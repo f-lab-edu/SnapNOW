62a85b40e02c4a98d4f48364a4362a31a5036223
김혜주 <sueelly@naver.com>
2024-12-04 18:53:45 +0900

    refactor(UserAccount): Include UserCredential instead of inheritance relation
    Related to #8

--- snapnow-domain/src/main/java/kr/flab/snapnow/domain/user/domain/model/userAccount/UserAccount.java
+++ snapnow-domain/src/main/java/kr/flab/snapnow/domain/user/domain/model/userAccount/UserAccount.java
@@ -4,64 +4,50 @@ import java.time.LocalDate;
 import java.util.Locale;
 
 import lombok.Getter;
-import lombok.AccessLevel;
 import lombok.Builder;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
 
-import kr.flab.snapnow.domain.user.domain.enums.AuthProvider;
 import kr.flab.snapnow.domain.user.domain.enums.Gender;
-import kr.flab.snapnow.domain.user.domain.exception.IdentityRequiredException;
-import kr.flab.snapnow.domain.user.domain.exception.InvalidEmailException;
-import kr.flab.snapnow.domain.user.domain.exception.InvalidPasswordException;
+import kr.flab.snapnow.domain.user.domain.enums.AuthProvider;
+import kr.flab.snapnow.domain.user.domain.exception.UserRequiredArgumentException;
 
 @Getter
 @Builder
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserAccount {
 
-    private Long id;
-    private String email;
-    private String password;
-    private AuthProvider authProvider;
-    private String providerId;
+    private UserCredential credential;
 
     private String name;
     private LocalDate birthDay;
     private String phoneNumber;
     private Gender gender;
     private Locale locale;
-    private boolean verifiedEmail;
 
-    public static class UserAccountBuilder {
-
-        private static final String EMAIL_REGEX = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
-
-        public UserAccount build() {
-            if (email == null || providerId == null) {
-                throw new IdentityRequiredException();
-            }
-            if (email != null && !email.matches(EMAIL_REGEX)) {
-                throw new InvalidEmailException();
-            }
-            if (password == null || password.isBlank()) {
-                throw new InvalidPasswordException();
-            }
-            if (locale == null) {
-                locale = Locale.KOREA;
-            }
-            if (gender == null) {
-                gender = Gender.UNKNOWN;
-            }
-
-            return new UserAccount(id, email, password, authProvider, providerId, name, birthDay,
-                    phoneNumber, gender, locale, verifiedEmail);
+    @Builder
+    protected UserAccount(UserCredential credential, String name, LocalDate birthDay,
+            String phoneNumber, Gender gender, Locale locale) {
+        if (credential == null) {
+            throw new UserRequiredArgumentException();
         }
+        this.credential = credential;
+        this.name = name;
+        this.birthDay = birthDay;
+        this.phoneNumber = phoneNumber;
+        this.gender = gender == null ? Gender.UNKNOWN : gender;
+        this.locale = locale == null ? Locale.KOREA : locale;
     }
 
-    public void updatePassword(String password) {
-        this.password = password;
+    @Builder
+    protected UserAccount(Long userId, String email, String password, AuthProvider authProvider,
+            String providerId, boolean verifiedEmail, String name, LocalDate birthDay,
+            String phoneNumber, Gender gender, Locale locale) {
+        this(UserCredential.builder()
+                .userId(userId)
+                .email(email)
+                .password(password)
+                .authProvider(authProvider)
+                .providerId(providerId)
+                .verifiedEmail(verifiedEmail)
+                .build(), name, birthDay, phoneNumber, gender, locale);
     }
 
     public void updateName(String name) {
@@ -82,9 +68,5 @@ public class UserAccount {
 
     public void updateLocale(Locale locale) {
         this.locale = locale;
-    }
-
-    public void updateVerifiedEmail() {
-        this.verifiedEmail = true;
     }
 }
