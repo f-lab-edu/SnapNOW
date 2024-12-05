e525037d7450d5a1a431f1ee8145632669f8b246
김혜주 <sueelly@naver.com>
2024-12-05 14:26:07 +0900

    feat(UserAccount): Has AccountInfo & UserCredential field not individual fields
    Related to #8

--- snapnow-domain/src/main/java/kr/flab/snapnow/domain/user/domain/model/userAccount/UserAccount.java
+++ snapnow-domain/src/main/java/kr/flab/snapnow/domain/user/domain/model/userAccount/UserAccount.java
@@ -1,72 +1,25 @@
 package kr.flab.snapnow.domain.user.domain.model.userAccount;
 
-import java.time.LocalDate;
-import java.util.Locale;
-
 import lombok.Getter;
 import lombok.Builder;
 
-import kr.flab.snapnow.domain.user.domain.enums.Gender;
-import kr.flab.snapnow.domain.user.domain.enums.AuthProvider;
 import kr.flab.snapnow.domain.user.domain.exception.UserRequiredArgumentException;
 
 @Getter
 @Builder
 public class UserAccount {
 
+    private Long userId;
     private UserCredential credential;
-
-    private String name;
-    private LocalDate birthDay;
-    private String phoneNumber;
-    private Gender gender;
-    private Locale locale;
+    private UserAccountInfo info;
 
     @Builder
-    protected UserAccount(UserCredential credential, String name, LocalDate birthDay,
-            String phoneNumber, Gender gender, Locale locale) {
-        if (credential == null) {
+    protected UserAccount(Long userId, UserCredential credential, UserAccountInfo info) {
+        if (credential == null || info == null) {
             throw new UserRequiredArgumentException();
         }
+        this.userId = userId;
         this.credential = credential;
-        this.name = name;
-        this.birthDay = birthDay;
-        this.phoneNumber = phoneNumber;
-        this.gender = gender == null ? Gender.UNKNOWN : gender;
-        this.locale = locale == null ? Locale.KOREA : locale;
-    }
-
-    @Builder
-    protected UserAccount(Long userId, String email, String password, AuthProvider authProvider,
-            String providerId, boolean verifiedEmail, String name, LocalDate birthDay,
-            String phoneNumber, Gender gender, Locale locale) {
-        this(UserCredential.builder()
-                .userId(userId)
-                .email(email)
-                .password(password)
-                .authProvider(authProvider)
-                .providerId(providerId)
-                .verifiedEmail(verifiedEmail)
-                .build(), name, birthDay, phoneNumber, gender, locale);
-    }
-
-    public void updateName(String name) {
-        this.name = name;
-    }
-
-    public void updateBirthDay(LocalDate birthDay) {
-        this.birthDay = birthDay;
-    }
-
-    public void updatePhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public void updateGender(Gender gender) {
-        this.gender = gender;
-    }
-
-    public void updateLocale(Locale locale) {
-        this.locale = locale;
+        this.info = info;
     }
 }
