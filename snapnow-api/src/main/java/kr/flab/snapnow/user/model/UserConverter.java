6ba72d76c0c321332b067044d39c4b8fb2b0bbbb
김혜주 <sueelly@naver.com>
2024-12-05 14:29:50 +0900

    refactor(UserApiConverter): Change to fit changed domain models

--- snapnow-api/src/main/java/kr/flab/snapnow/user/model/UserConverter.java
+++ snapnow-api/src/main/java/kr/flab/snapnow/user/model/UserConverter.java
@@ -2,13 +2,17 @@ package kr.flab.snapnow.user.model;
 
 import java.util.List;
 import java.util.stream.Collectors;
-
+import kr.flab.snapnow.domain.user.domain.enums.AuthProvider;
 import kr.flab.snapnow.domain.user.domain.model.User;
+import kr.flab.snapnow.domain.user.domain.model.userAccount.Email;
+import kr.flab.snapnow.domain.user.domain.model.userAccount.EmailCredential;
+import kr.flab.snapnow.domain.user.domain.model.userAccount.OAuthCredential;
+import kr.flab.snapnow.domain.user.domain.model.userAccount.Password;
 import kr.flab.snapnow.domain.user.domain.model.userAccount.UserAccount;
+import kr.flab.snapnow.domain.user.domain.model.userAccount.UserAccountInfo;
 import kr.flab.snapnow.domain.user.domain.model.userAccount.UserCredential;
 import kr.flab.snapnow.domain.user.domain.model.userProfile.UserProfile;
 import kr.flab.snapnow.domain.user.domain.model.userProfile.FullProfile;
-import kr.flab.snapnow.domain.user.domain.model.userProfile.CompactProfileWithFollowStatus;
 import kr.flab.snapnow.domain.user.domain.model.userSetting.NotificationSettings;
 import kr.flab.snapnow.domain.user.domain.model.userSetting.PrivacySettings;
 
@@ -25,13 +29,13 @@ public class UserConverter {
 
     public static AccountResponse toAccountResponse(UserAccount userAccount) {
         return AccountResponse.builder()
-                .email(userAccount.getCredential().getEmail())
+                .email(userAccount.getCredential().getEmail().getValue())
                 .authProvider(userAccount.getCredential().getAuthProvider())
-                .name(userAccount.getName())
-                .birthDay(userAccount.getBirthDay())
-                .phoneNumber(userAccount.getPhoneNumber())
-                .gender(userAccount.getGender())
-                .locale(userAccount.getLocale())
+                .name(userAccount.getInfo().getName())
+                .birthDay(userAccount.getInfo().getBirthDay())
+                .phoneNumber(userAccount.getInfo().getPhoneNumber())
+                .gender(userAccount.getInfo().getGender())
+                .locale(userAccount.getInfo().getLocale())
                 .build();
     }
 
@@ -44,11 +48,13 @@ public class UserConverter {
                 .postCount(fullProfile.getPostCount())
                 .followerCount(fullProfile.getFollowerCount())
                 .followingCount(fullProfile.getFollowingCount())
-                .followStatus(fullProfile.getFollowStatus()).build();
+                .followStatus(fullProfile.getFollow().getFollowStatus())
+                .followNotification(fullProfile.getFollow().getFollowNotification())
+                .build();
     }
 
     public static ProfilesResponse toProfilesResponse(
-            List<CompactProfileWithFollowStatus> userProfiles) {
+            List<UserProfile> userProfiles) {
         return ProfilesResponse.builder()
                 .profiles(userProfiles.stream()
                         .map(UserConverter::toProfileElement).collect(Collectors.toList()))
@@ -64,35 +70,57 @@ public class UserConverter {
     }
 
     public static PrivacySettingResponse toPrivacySettingResponse(PrivacySettings privacySetting) {
-            return PrivacySettingResponse.builder().category(privacySetting.getCategory())
-                            .settings(privacySetting.getSettings()).build();
+        return PrivacySettingResponse.builder()
+                .category(privacySetting.getCategory())
+                .settings(privacySetting.getSettings())
+                .build();
     }
 
     public static User toUser(UserCreateRequest userCreateRequest, String profileImageUrl) {
-        UserCredential userCredential = UserCredential.builder()
-                .email(userCreateRequest.getEmail())
-                .password(userCreateRequest.getPassword())
-                .providerId(userCreateRequest.getOAuthToken())
-                .build();
-        UserAccount userAccount = UserAccount.builder()
-                .credential(userCredential)
+        Email email = new Email(userCreateRequest.getEmail());
+        Password password = new Password(userCreateRequest.getPassword());
+        UserCredential userCredential;
+
+        if (userCreateRequest.getOAuthToken() != null) {
+                // Todo: jwt로 AuthProvider 추출
+            userCredential = OAuthCredential.builder()
+                    .email(email)
+                    .providerId(userCreateRequest.getOAuthToken())
+                    .authProvider(AuthProvider.GOOGLE)
+                    .build();
+        } else {
+            userCredential = EmailCredential.builder()
+                    .email(email)
+                    .password(password)
+                    .build();
+        }
+        UserAccountInfo userAccountInfo = UserAccountInfo.builder()
                 .name(userCreateRequest.getName())
                 .gender(userCreateRequest.getGender())
                 .locale(userCreateRequest.getLocale())
                 .birthDay(userCreateRequest.getBirthDay())
                 .phoneNumber(userCreateRequest.getPhoneNumber())
                 .build();
+
+        UserAccount userAccount = UserAccount.builder()
+                .credential(userCredential)
+                .info(userAccountInfo)
+                .build();
+
         UserProfile userProfile = UserProfile.builder()
                 .userName(userCreateRequest.getUserName())
                 .fullName(userCreateRequest.getFullName())
                 .biography(userCreateRequest.getBiography())
                 .profileImageUrl(profileImageUrl)
                 .build();
-        return User.builder().account(userAccount).profile(userProfile).build();
+
+        return User.builder()
+                .account(userAccount)
+                .profile(userProfile)
+                .build();
     }
 
-    private static ProfileElement toProfileElement(
-            CompactProfileWithFollowStatus userProfile) {
+    private static ProfileElement toProfileElement(UserProfile userProfile) {
         return ProfileElement.builder()
                 .userId(userProfile.getUserId())
                 .userName(userProfile.getUserName())
