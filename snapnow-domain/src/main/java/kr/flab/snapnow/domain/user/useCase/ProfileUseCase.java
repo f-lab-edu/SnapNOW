a8583ac2233c9f0c1c88e504cd89e837114124da
김혜주 <sueelly@naver.com>
2024-12-05 11:59:59 +0900

    refactor(ProfileUseCase): Remove useless mehtods and add return class of update mehtod
    Related to #50

--- snapnow-domain/src/main/java/kr/flab/snapnow/domain/user/useCase/ProfileUseCase.java
+++ snapnow-domain/src/main/java/kr/flab/snapnow/domain/user/useCase/ProfileUseCase.java
@@ -1,16 +1,13 @@
 package kr.flab.snapnow.domain.user.useCase;
 
-import kr.flab.snapnow.domain.user.domain.model.userProfile.CompactProfile;
 import kr.flab.snapnow.domain.user.domain.model.userProfile.FullProfile;
 import kr.flab.snapnow.domain.user.domain.model.userProfile.UserProfile;
 
 public interface ProfileUseCase {
 
-    public CompactProfile getCompactProfile(Long userId);
-
     public FullProfile getProfile(Long userId);
 
     public FullProfile getProfile(Long targetId, Long userId);
 
-    public void updateProfile(UserProfile userProfile);
+    public UserProfile updateProfile(UserProfile userProfile);
 }
