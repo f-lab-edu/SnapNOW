182e3bf7775fe30f05508119c5195d7f1e4d80de
김혜주 <sueelly@naver.com>
2024-12-11 03:28:47 +0900

    feat(UserSetting): Create UserSettings classes

---
+++ snapnow-domain/src/main/java/kr/flab/snapnow/domain/user/domain/model/userSetting/PrivacySettings.java
@@ -0,0 +1,33 @@
+package kr.flab.snapnow.domain.user.domain.model.userSetting;
+
+import java.util.Map;
+import java.util.HashMap;
+
+import lombok.Builder;
+
+import kr.flab.snapnow.domain.user.domain.enums.setting.SettingCategory;
+import kr.flab.snapnow.domain.user.domain.enums.setting.settingOption.PrivacySettingOption;
+import kr.flab.snapnow.domain.user.domain.enums.setting.settingType.PrivacySettingType;
+
+@Builder
+public class PrivacySettings extends UserSettings<PrivacySettingType, PrivacySettingOption> {
+
+    public PrivacySettings() {
+        super(null, SettingCategory.PRIVACY, new HashMap<>());
+
+        this.category.getSettingType().forEach(settingType -> {
+            this.settings.put((PrivacySettingType) settingType,
+                (PrivacySettingOption) this.category.getDefaultSettingOption());
+        });
+    }
+
+    @Builder
+    private PrivacySettings(Long userId, SettingCategory category,
+            Map<PrivacySettingType, PrivacySettingOption> settings) {
+        super(userId, category, settings);
+        if (!SettingCategory.PRIVACY.equals(category)) {
+            throw new IllegalArgumentException("Invalid setting category");
+        }
+        
+    }
+}
