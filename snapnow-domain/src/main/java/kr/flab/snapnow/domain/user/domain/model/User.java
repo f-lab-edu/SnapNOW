f929ff60b16644641cfcb4c07817c28da607530a
김혜주 <sueelly@naver.com>
2024-12-11 03:51:32 +0900

    feat(User): Create User class for save

---
+++ snapnow-domain/src/main/java/kr/flab/snapnow/domain/user/domain/model/User.java
@@ -0,0 +1,50 @@
+package kr.flab.snapnow.domain.user.domain.model;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import lombok.Getter;
+import lombok.Builder;
+
+import kr.flab.snapnow.domain.user.domain.model.userAccount.UserAccount;
+import kr.flab.snapnow.domain.user.domain.model.userProfile.UserProfile;
+import kr.flab.snapnow.domain.user.domain.model.userSetting.UserSettings;
+import kr.flab.snapnow.domain.user.domain.model.userSetting.NotificationSettings;
+import kr.flab.snapnow.domain.user.domain.model.userSetting.PrivacySettings;
+import kr.flab.snapnow.domain.user.domain.model.UserDevice.UserDevice;
+
+@Getter
+@Builder
+public class User {
+
+    private Long userId;
+    private UserAccount account;
+    private UserProfile profile;
+    private List<UserSettings> settings;
+    private UserDevice userDevice;
+
+    private User(Long userId, UserAccount account, UserProfile profile,
+            List<UserSettings> settings, UserDevice userDevice) {
+        if (account == null) {
+            throw new IllegalArgumentException("Account is required");
+        }
+        if (profile == null) {
+            throw new IllegalArgumentException("Profile is required");
+        }
+        if (userDevice == null) {
+            throw new IllegalArgumentException("User device is required");
+        }
+        if (settings == null) {
+            settings = new ArrayList<>();
+            settings.add(new NotificationSettings());
+            settings.add(new PrivacySettings());
+        }
+
+        this.userId = userId;
+        this.account = account;
+        this.profile = profile;
+        this.settings = settings;
+        this.userDevice = userDevice;
+    }
+}
+
