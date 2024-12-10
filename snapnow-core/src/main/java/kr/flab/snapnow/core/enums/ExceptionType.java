8cab0f5cbf5a260f5cf9f3c35f7b6cc29d38f458
김혜주 <sueelly@naver.com>
2024-12-10 22:06:14 +0900

    feat(ResultCode): Create ExceptionType and SuccessCode enum

---
+++ snapnow-core/src/main/java/kr/flab/snapnow/core/enums/ExceptionType.java
@@ -0,0 +1,16 @@
+package kr.flab.snapnow.core.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ExceptionType implements ResultCode {
+
+    BAD_REQUEST("Bad Request"),
+    UNAUTHORIZED("Unauthorized request"),
+    NOT_FOUND("Resource not found"),
+    ;
+
+    private final String message;
+}
