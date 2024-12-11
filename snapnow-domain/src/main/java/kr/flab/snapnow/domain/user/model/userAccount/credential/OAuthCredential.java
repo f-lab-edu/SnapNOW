975638bd9749f984c6e6dfd22dbe2b427fd4b0a5
김혜주 <sueelly@naver.com>
2024-12-12 03:24:35 +0900

    fix(UserCredential): Fix that Superbuilder doesn't use constructor...

--- snapnow-domain/src/main/java/kr/flab/snapnow/domain/user/model/userAccount/credential/OAuthCredential.java
+++ snapnow-domain/src/main/java/kr/flab/snapnow/domain/user/model/userAccount/credential/OAuthCredential.java
@@ -1,22 +1,29 @@
 package kr.flab.snapnow.domain.user.model.userAccount.credential;
 
 import lombok.Getter;
-import lombok.experimental.SuperBuilder;
+import lombok.Builder;
 
 import kr.flab.snapnow.domain.user.enums.account.AuthProvider;
 
 @Getter
-@SuperBuilder
+@Builder
 public class OAuthCredential extends UserCredential {
 
+    private Long userId;
+    private AuthProvider authProvider;
+    private Email email;
     private String providerId;
 
-    private OAuthCredential(Long userId, AuthProvider authProvider, String providerId,
-            Email email) {
+    private OAuthCredential(
+        Long userId, AuthProvider authProvider, Email email, String providerId) {
         super(userId, authProvider, email);
+
         if (providerId == null) {
             throw new IllegalArgumentException("Provider ID is required");
         }
+        this.userId = userId;
+        this.authProvider = authProvider;
+        this.email = email;
         this.providerId = providerId;
     }
 }
