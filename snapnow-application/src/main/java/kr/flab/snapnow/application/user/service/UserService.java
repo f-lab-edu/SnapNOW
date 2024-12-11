bb3e73bc199b40bb58f0399ae97e753e95dc8f50
김혜주 <sueelly@naver.com>
2024-12-11 22:41:54 +0900

    refactor(UserServie): change use of AccountService to CredentialService

--- snapnow-application/src/main/java/kr/flab/snapnow/application/user/service/UserService.java
+++ snapnow-application/src/main/java/kr/flab/snapnow/application/user/service/UserService.java
@@ -3,10 +3,11 @@ package kr.flab.snapnow.application.user.service;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.stereotype.Service;
+
 import kr.flab.snapnow.core.exception.BadRequestException;
 import kr.flab.snapnow.core.exception.ForbiddenException;
-import kr.flab.snapnow.domain.auth.exception.UserNotMatchPasswordException;
 import kr.flab.snapnow.domain.auth.Token;
+import kr.flab.snapnow.domain.auth.exception.UserNotMatchPasswordException;
 import kr.flab.snapnow.domain.user.enums.account.AuthProvider;
 import kr.flab.snapnow.domain.user.model.User;
 import kr.flab.snapnow.domain.user.model.userAccount.credential.Email;
@@ -16,6 +17,7 @@ import kr.flab.snapnow.application.user.usecase.DeleteIdUseCase;
 import kr.flab.snapnow.application.user.usecase.SignUpUseCase;
 import kr.flab.snapnow.application.user.output.UserOutputPort;
 import kr.flab.snapnow.application.auth.service.AuthService;
+import kr.flab.snapnow.application.auth.service.CredentialService;
 import kr.flab.snapnow.application.email.VerificationType;
 import kr.flab.snapnow.application.email.service.EmailService;
 
@@ -24,8 +26,8 @@ import kr.flab.snapnow.application.email.service.EmailService;
 public class UserService implements SignUpUseCase, DeleteIdUseCase {
 
     private final AuthService authService;
+    private final CredentialService credentialService;
     private final EmailService emailService;
-    private final UserAccountService userAccountService;
     private final UserOutputPort userOutputPort;
 
     public Token signUp(User user) {
@@ -35,19 +37,19 @@ public class UserService implements SignUpUseCase, DeleteIdUseCase {
         if (!emailService.isSuccess(
                 email, VerificationType.SIGNUP)) {
             throw new ForbiddenException("Email verification is needed before signing up");
-        }   
+        }
 
         userOutputPort.insert(user);
         return authService.issue(email, device.getDeviceId());
     }
 
     public void deleteEmailUser(Long userId, String password, String deleteReason) {
-        UserCredential credential = userAccountService.getCredential(userId);
+        UserCredential credential = credentialService.getCredential(userId);
 
         if (credential.getAuthProvider() != AuthProvider.EMAIL) {
             throw new BadRequestException("This request is not allowed for OAuth users");
         }
-        if (!authService.isPasswordMatch(userId, password)) {
+        if (!credentialService.isPasswordMatch(userId, password)) {
             throw new UserNotMatchPasswordException();
         }
 
@@ -55,7 +57,7 @@ public class UserService implements SignUpUseCase, DeleteIdUseCase {
     }
 
     public void deleteOAuthUser(Long userId, String deleteReason) {
-        UserCredential credential = userAccountService.getCredential(userId);
+        UserCredential credential = credentialService.getCredential(userId);
 
         if (credential.getAuthProvider() == AuthProvider.EMAIL) {
             throw new BadRequestException("This request is not allowed for email users");
