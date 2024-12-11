ebfe94036684340ca82d6624d3f0e6d262901996
김혜주 <sueelly@naver.com>
2024-12-12 03:26:22 +0900

    fix(UserDevice): Add init field logic

--- snapnow-domain/src/main/java/kr/flab/snapnow/domain/user/model/UserDevice/UserDevice.java
+++ snapnow-domain/src/main/java/kr/flab/snapnow/domain/user/model/UserDevice/UserDevice.java
@@ -14,12 +14,14 @@ public class UserDevice {
 
     @Builder
     private UserDevice(Long userId, List<Device> devices) {
-        if (devices == null) {
+        if (devices == null || devices.isEmpty()) {
             throw new IllegalArgumentException("Devices are required");
         }
         if (devices.size() > 5) {
             throw new IllegalArgumentException("Device count exceeded (Max 5)");
         }
+        this.userId = userId;
+        this.devices = devices;
     }
 }
 
