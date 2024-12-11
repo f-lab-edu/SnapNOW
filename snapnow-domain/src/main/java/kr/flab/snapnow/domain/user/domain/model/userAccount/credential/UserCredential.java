6bdb6fb8e6755b18bf964d40b2883fb7774679df
김혜주 <sueelly@naver.com>
2024-12-11 18:09:08 +0900

    feat(UserCredential): Change Builder to SuperBuilder

--- snapnow-domain/src/main/java/kr/flab/snapnow/domain/user/domain/model/userAccount/credential/UserCredential.java
+++ snapnow-domain/src/main/java/kr/flab/snapnow/domain/user/domain/model/userAccount/credential/UserCredential.java
@@ -1,9 +1,12 @@
 package kr.flab.snapnow.domain.user.domain.model.userAccount.credential;
 
-import kr.flab.snapnow.domain.user.domain.enums.account.AuthProvider;
 import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+
+import kr.flab.snapnow.domain.user.domain.enums.account.AuthProvider;
 
 @Getter
+@SuperBuilder
 public abstract class UserCredential {
 
     protected AuthProvider authProvider;
