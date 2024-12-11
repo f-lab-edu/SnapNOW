830329a127f5576e28ccf7b43f4158f14ba82c0d
김혜주 <sueelly@naver.com>
2024-12-11 21:31:46 +0900

    feat(Auth): Create AuthService

---
+++ snapnow-application/src/main/java/kr/flab/snapnow/application/auth/service/AuthService.java
@@ -0,0 +1,35 @@
+package kr.flab.snapnow.application.auth.service;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.stereotype.Service;
+
+import kr.flab.snapnow.domain.auth.Token;
+import kr.flab.snapnow.domain.user.model.userAccount.credential.Email;
+import kr.flab.snapnow.application.auth.usecase.AuthUseCase;
+import kr.flab.snapnow.application.auth.output.AuthOutputPort;
+import kr.flab.snapnow.application.auth.output.TokenOutputPort;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService implements AuthUseCase {
+
+    private final AuthOutputPort authOutputPort;
+    private final TokenOutputPort tokenOutputPort;
+
+    public Token signIn(Email email, String password, String deviceId) {
+        return null;
+    }
+
+    public Token issue(Email email, String deviceId) {
+        return null;
+    }
+
+    public Token reissue(Token token, String deviceId) {
+        return null;
+    }
+
+    public void signOut(Email email, String deviceId) {
+    }
+}
+
