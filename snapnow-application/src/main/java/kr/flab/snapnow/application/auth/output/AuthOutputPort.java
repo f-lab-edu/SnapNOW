59a21dee880a0d782c6c8fcc9ca229e16664aff9
김혜주 <sueelly@naver.com>
2024-12-11 22:37:23 +0900

    rename(AuthOutput): Remove AuthOutputPort and create CredentialOutputPort
    Move accountOutputPort method's to Credential (getCredential & updatePassword)

--- snapnow-application/src/main/java/kr/flab/snapnow/application/auth/output/AuthOutputPort.java
+++
@@ -1,9 +0,0 @@
-package kr.flab.snapnow.application.auth.output;
-
-import kr.flab.snapnow.domain.user.model.userAccount.credential.Email;
-import kr.flab.snapnow.domain.user.model.userAccount.credential.UserCredential;
-
-public interface AuthOutputPort {
-
-    UserCredential getCredential(Email email);
-}
