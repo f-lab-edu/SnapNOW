8f106df6b1f74d9ebd1d1773a06cdae5fc882d79
김혜주 <sueelly@naver.com>
2024-12-11 17:58:12 +0900

    feat(UserTest): Create UserService test

---
+++ snapnow-application/src/test/java/kr/flab/snapnow/application/user/service/UserSerivceT.java
@@ -0,0 +1,57 @@
+package kr.flab.snapnow.application.user.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mock;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import kr.flab.snapnow.core.exception.ForbiddenException;
+import kr.flab.snapnow.domain.user.model.User;
+import kr.flab.snapnow.application.user.output.UserOutputPort;
+import kr.flab.snapnow.application.email.output.EmailVerificationOutputPort;
+import kr.flab.snapnow.application.user.fixture.UserFixture;
+
+public class UserSerivceT {
+
+    @Mock
+    private UserOutputPort userOutputPort;
+    @Mock
+    private EmailVerificationOutputPort emailVerificationOutputPort;
+    @InjectMocks
+    private UserService userService;
+
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testSignUp() {
+        // given
+        User user = UserFixture.createUser();
+
+        // when
+        userService.signUp(user);
+
+        // then
+        verify(userOutputPort).insert(user);
+    }
+
+    @Test
+    public void failSignUpEmailNotVerified() {
+        // given
+        User user = UserFixture.createUser();
+        when(emailVerificationOutputPort.isSuccess(
+            user.getAccount().getCredential().getEmail().getValue(),
+            "Verification Code")).thenReturn(false);
+
+        // when & then
+        assertThrows(ForbiddenException.class, () -> userService.signUp(user));
+    }
+}
