8c17594e0f5e55740acb6a8828fe39925bbc950e
김혜주 <sueelly@naver.com>
2024-12-11 22:06:27 +0900

    feat(UserService): Implements delete methods (divides email & OAuth user)

--- snapnow-application/src/main/java/kr/flab/snapnow/application/user/service/UserService.java
+++ snapnow-application/src/main/java/kr/flab/snapnow/application/user/service/UserService.java
@@ -3,11 +3,14 @@ package kr.flab.snapnow.application.user.service;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.stereotype.Service;
-
+import kr.flab.snapnow.core.exception.BadRequestException;
 import kr.flab.snapnow.core.exception.ForbiddenException;
+import kr.flab.snapnow.domain.auth.exception.UserNotMatchPasswordException;
 import kr.flab.snapnow.domain.auth.Token;
+import kr.flab.snapnow.domain.user.enums.account.AuthProvider;
 import kr.flab.snapnow.domain.user.model.User;
 import kr.flab.snapnow.domain.user.model.userAccount.credential.Email;
+import kr.flab.snapnow.domain.user.model.userAccount.credential.UserCredential;
 import kr.flab.snapnow.domain.user.model.userDevice.Device;
 import kr.flab.snapnow.application.user.usecase.DeleteIdUseCase;
 import kr.flab.snapnow.application.user.usecase.SignUpUseCase;
@@ -22,6 +25,7 @@ public class UserService implements SignUpUseCase, DeleteIdUseCase {
 
     private final AuthService authService;
     private final EmailService emailService;
+    private final UserAccountService userAccountService;
     private final UserOutputPort userOutputPort;
 
     public Token signUp(User user) {
@@ -37,4 +41,35 @@ public class UserService implements SignUpUseCase, DeleteIdUseCase {
         return authService.issue(email, device.getDeviceId());
     }
 
+    public void deleteEmailUser(Long userId, String password, String deleteReason) {
+        UserCredential credential = userAccountService.getCredential(userId);
+
+        if (credential.getAuthProvider() != AuthProvider.EMAIL) {
+            throw new BadRequestException("This request is not allowed for OAuth users");
+        }
+        if (!authService.isPasswordMatch(userId, password)) {
+            throw new UserNotMatchPasswordException();
+        }
+
+        delete(userId, deleteReason);
+    }
+
+    public void deleteOAuthUser(Long userId, String deleteReason) {
+        UserCredential credential = userAccountService.getCredential(userId);
+
+        if (credential.getAuthProvider() == AuthProvider.EMAIL) {
+            throw new BadRequestException("This request is not allowed for email users");
+        }
+        if (!emailService.isSuccess(credential.getEmail(), VerificationType.DELETE_ID)) {
+            throw new ForbiddenException(
+                    "Email verification is needed before deleting OAuth account");
+        }
+
+        delete(userId, deleteReason);
+    }
+
+    private void delete(Long userId, String deleteReason) {
+        userOutputPort.delete(userId, deleteReason);
+    }
 }
+
