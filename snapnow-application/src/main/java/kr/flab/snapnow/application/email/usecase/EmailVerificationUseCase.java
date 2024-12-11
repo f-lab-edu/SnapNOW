3a443d6c942bac265b0a28b1855558ac6ad05f47
김혜주 <sueelly@naver.com>
2024-12-11 21:29:17 +0900

    feat(EmailService): Change String to Email Vo and add VerificationType param to methods

--- snapnow-application/src/main/java/kr/flab/snapnow/application/email/usecase/EmailVerificationUseCase.java
+++ snapnow-application/src/main/java/kr/flab/snapnow/application/email/usecase/EmailVerificationUseCase.java
@@ -1,8 +1,12 @@
 package kr.flab.snapnow.application.email.usecase;
 
-public interface EmailVerificationUseCase {
-    
-    public void sendVerificationCode(String email);
+import kr.flab.snapnow.domain.user.model.userAccount.credential.Email;
+import kr.flab.snapnow.application.email.VerificationType;
 
-    public boolean verifyVerificationCode(String email, String code);
+public interface EmailVerificationUseCase {
+
+    void sendVerificationCode(Email email, VerificationType type);
+
+    boolean verifyVerificationCode(Email email, String code, VerificationType type);
 }
+
