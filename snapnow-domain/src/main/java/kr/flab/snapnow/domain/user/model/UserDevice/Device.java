fa5be9926224f2ddb71685ad5744503d9bd41e6b
김혜주 <sueelly@naver.com>
2024-12-24 00:39:29 +0900

    refactor(Device): Remove refreshToken and add deviceToken for notification

--- snapnow-domain/src/main/java/kr/flab/snapnow/domain/user/model/UserDevice/Device.java
+++ snapnow-domain/src/main/java/kr/flab/snapnow/domain/user/model/UserDevice/Device.java
@@ -9,21 +9,26 @@ import lombok.Getter;
 public class Device {
 
     private String deviceId;
+    private String deviceToken;
+
     private DeviceType deviceType;
     private String deviceModel;
     private String deviceOs;
     private String deviceOsVersion;
 
-    private String refreshToken;
-
     @Builder
-    private Device(String deviceId, DeviceType deviceType, String deviceModel,
-            String deviceOs, String deviceOsVersion, String refreshToken) {
+    private Device(String deviceId, String deviceToken,
+            DeviceType deviceType, String deviceModel, String deviceOs, String deviceOsVersion) {
         if (deviceId == null) {
             throw new IllegalArgumentException("Device ID is required");
         }
         if (deviceType == null) {
             throw new IllegalArgumentException("Device type is required");
         }
+        // TODO: 디바이스 토큰 필수. 현재 구현이 안되어있어 생략...
+    }
+
+    public void updateDeviceToken(String deviceToken) {
+        this.deviceToken = deviceToken;
     }
 }
