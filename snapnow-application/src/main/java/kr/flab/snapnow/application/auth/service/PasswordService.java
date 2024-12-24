3bd9c89d25e95015e2a92b2707173a26030f57d1
김혜주 <sueelly@naver.com>
2024-12-24 22:19:34 +0900

    feat(PasswordSerivce): Implement updatePassword method and add password setter to EmailCredential
    Related to #62

--- snapnow-application/src/main/java/kr/flab/snapnow/application/auth/service/PasswordService.java
+++ snapnow-application/src/main/java/kr/flab/snapnow/application/auth/service/PasswordService.java
@@ -1,32 +1,49 @@
 package kr.flab.snapnow.application.auth.service;
 
-import lombok.RequiredArgsConstructor;
-
 import org.springframework.stereotype.Service;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import kr.flab.snapnow.domain.auth.exception.InvalidPasswordException;
+import kr.flab.snapnow.domain.auth.exception.WrongPasswordException;
 import kr.flab.snapnow.domain.user.model.userAccount.credential.EmailCredential;
 
 @Service
-@RequiredArgsConstructor
 public class PasswordService {
 
-    private final BCryptPasswordEncoder passwordEncoder;
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    public String validateAndEncodePassword(String password) {
+        if (!isValidPassword(password)) {
+            throw new InvalidPasswordException();
+        }
+
+        return encodePassword(password);
+    }
+
+    public void updatePassword(
+        EmailCredential credential, String originalPassword, String newPassword) {
+        if (!isPasswordMatch(originalPassword, credential.getPassword())) {
+            throw new WrongPasswordException();
+        }
+        if (!isValidPassword(newPassword)) {
+            throw new InvalidPasswordException();
+        }
+
+        String encodedNewPassword = encodePassword(newPassword);
+        credential.updatePassword(encodedNewPassword);
+    }
 
     public boolean isValidPassword(String password) {
-        return password.length() >= 8
+        return password.length() >= 8 && password.length() <= 15
                 && password.matches(".*[a-zA-Z].*")
                 && password.matches(".*\\d.*");
     }
 
     public String encodePassword(String password) {
-        return null;
+        return passwordEncoder.encode(password);
     }
 
-    public boolean isPasswordMatch(String password, String passwordToMatch) {
-        return false;
-    }
-
-    public void updatePassword(EmailCredential credential, String newPassword) {
+    public boolean isPasswordMatch(String passwordToMatch, String password) {
+        return passwordEncoder.matches(passwordToMatch, password);
     }
 }
