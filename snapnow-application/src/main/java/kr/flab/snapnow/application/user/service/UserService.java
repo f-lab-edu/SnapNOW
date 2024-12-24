282990bc8217289ac3d4103061294098bf4166c0
김혜주 <sueelly@naver.com>
2024-12-24 23:38:47 +0900

    refactor(UserService): Add @Transactional
    Related to #64

--- snapnow-application/src/main/java/kr/flab/snapnow/application/user/service/UserService.java
+++ snapnow-application/src/main/java/kr/flab/snapnow/application/user/service/UserService.java
@@ -3,6 +3,7 @@ package kr.flab.snapnow.application.user.service;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import kr.flab.snapnow.core.exception.BadRequestException;
 import kr.flab.snapnow.core.exception.ForbiddenException;
@@ -21,6 +22,7 @@ import kr.flab.snapnow.application.email.VerificationType;
 import kr.flab.snapnow.application.email.service.EmailService;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class UserService implements SignUpUseCase, DeleteIdUseCase {
 
