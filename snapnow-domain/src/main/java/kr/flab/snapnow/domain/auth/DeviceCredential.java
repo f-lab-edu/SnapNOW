a62a4b41fd9e6ed3051cd17f098ab87dd0294b5b
김혜주 <sueelly@naver.com>
2024-12-25 16:13:17 +0900

    feat(DeviceCredential): Add expriedAt field and add check logic in login/logout/reissue
    Reltaed to #64

--- snapnow-domain/src/main/java/kr/flab/snapnow/domain/auth/DeviceCredential.java
+++ snapnow-domain/src/main/java/kr/flab/snapnow/domain/auth/DeviceCredential.java
@@ -8,6 +8,7 @@ import lombok.Builder;
 
 import kr.flab.snapnow.domain.auth.enums.LoginStatus;
 import kr.flab.snapnow.domain.auth.exception.LogoutDeviceException;
+import kr.flab.snapnow.domain.auth.exception.ExpiredTokenException;
 
 @Getter
 @Builder
@@ -17,11 +18,12 @@ public class DeviceCredential {
     private Long userId;
     private String deviceId;
     private String refreshToken;
+    private LocalDateTime expriedAt;
     private LoginStatus loginStatus;
     private LocalDateTime logTime;
 
     private DeviceCredential(Long id, Long userId, String deviceId,
-            String refreshToken, LoginStatus loginStatus, LocalDateTime logTime) {
+            String refreshToken, LocalDateTime expriedAt, LoginStatus loginStatus, LocalDateTime logTime) {
         if (userId == null) {
             throw new IllegalArgumentException("Device Credentail requires User ID");
         }
@@ -37,27 +39,35 @@ public class DeviceCredential {
         this.userId = userId;
         this.deviceId = deviceId;
         this.refreshToken = refreshToken;
+        this.expriedAt = expriedAt;
         this.loginStatus = loginStatus;
         this.logTime = logTime;
     }
 
-    public void login(String refreshToken) {
+    public void login(String refreshToken, LocalDateTime expriedAt) {
         this.refreshToken = refreshToken;
+        this.expriedAt = expriedAt;
         this.loginStatus = LoginStatus.LOGIN;
         this.logTime = LocalDateTime.now();
     }
 
     public void logout() {
         this.refreshToken = null;
+        this.expriedAt = null;
         this.loginStatus = LoginStatus.LOGOUT;
         this.logTime = LocalDateTime.now();
     }
 
-    public void reissue(String refreshToken) {
+    public void reissue(String refreshToken, LocalDateTime expriedAt) {
         if (this.loginStatus == LoginStatus.LOGOUT) {
             throw new LogoutDeviceException();
         }
+        if (this.expriedAt.isBefore(LocalDateTime.now())) {
+            throw new ExpiredTokenException();
+        }
+
         this.refreshToken = refreshToken;
+        this.expriedAt = expriedAt;
     }
 
     @Override
@@ -68,7 +78,11 @@ public class DeviceCredential {
 
         DeviceCredential that = (DeviceCredential) obj;
         return Objects.equals(userId, that.userId) &&
-                Objects.equals(deviceId, that.deviceId);
+                Objects.equals(deviceId, that.deviceId) &&
+                Objects.equals(refreshToken, that.refreshToken) &&
+                Objects.equals(expriedAt, that.expriedAt) &&
+                Objects.equals(loginStatus, that.loginStatus) &&
+                Objects.equals(logTime, that.logTime);
     }
 }
 
