4ed5995f9e841563497f9ca8439d5f1cf080f4d8
김혜주 <sueelly@naver.com>
2025-01-07 17:23:07 +0900

    refactor(AuthServie): Apply using token payload when issue

--- snapnow-application/src/main/java/kr/flab/snapnow/application/auth/service/AuthService.java
+++ snapnow-application/src/main/java/kr/flab/snapnow/application/auth/service/AuthService.java
@@ -1,6 +1,5 @@
 package kr.flab.snapnow.application.auth.service;
 
-import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
 
@@ -11,11 +10,11 @@ import org.springframework.transaction.annotation.Transactional;
 
 import kr.flab.snapnow.domain.user.model.userAccount.credential.Email;
 import kr.flab.snapnow.domain.auth.Token;
+import kr.flab.snapnow.domain.auth.exception.LogoutDeviceException;
 import kr.flab.snapnow.domain.auth.exception.InvalidTokenException;
 import kr.flab.snapnow.domain.auth.exception.WrongPasswordException;
 import kr.flab.snapnow.application.auth.usecase.dto.EmailSignInRequest;
 import kr.flab.snapnow.application.auth.usecase.dto.OAuthSignInRequest;
-import kr.flab.snapnow.application.auth.usecase.dto.IssueRequest;
 import kr.flab.snapnow.application.auth.usecase.dto.ReissueRequest;
 import kr.flab.snapnow.application.auth.usecase.dto.SignOutRequest;
 import kr.flab.snapnow.domain.user.model.userAccount.credential.EmailCredential;
@@ -45,17 +44,22 @@ public class AuthService implements AuthUseCase {
 
     public Token signIn(OAuthSignInRequest request) {
         OAuthCredential credential = credentialService.get(request.getProviderId());
+
         return signIn(credential.getUserId(), request.getDeviceId());
     }
 
     public Token signIn(Long userId, String deviceId) {
-        Token token = issue(IssueRequest.builder()
+        TokenPayload payload = TokenPayload.builder()
             .userId(userId)
             .deviceId(deviceId)
-            .build());
+            .issuedAt(new Date())
+            .build();
+        Token token = jwtProvider.createToken(payload);
 
-        deviceCredentialService.login(userId, deviceId, token.getRefreshToken());
-
+        deviceCredentialService.login(userId, deviceId, token.getRefreshToken(),
+                payload.getExpiredAt().toInstant()
+                .atZone(ZoneId.systemDefault())
+                .toLocalDateTime());
         return token;
     }
 
@@ -71,13 +75,18 @@ public class AuthService implements AuthUseCase {
             throw new LogoutDeviceException();
         }
 
-        Token newToken = issue(payload.getUserId(), payload.getDeviceId());
-        TokenPayload newPayload = jwtProvider.getPayload(newToken.getRefreshToken());
-        LocalDateTime newExpiredAt = newPayload.getExpiredAt().toInstant()
+        TokenPayload newPayload = TokenPayload.builder()
+            .userId(payload.getUserId())
+            .deviceId(payload.getDeviceId())
+            .issuedAt(new Date())
+                .build();
+        Token newToken = jwtProvider.createToken(newPayload);
+
+        deviceCredentialService.reissue(
+            payload.getUserId(), payload.getDeviceId(), newToken.getRefreshToken(),
+            newPayload.getExpiredAt().toInstant()
                 .atZone(ZoneId.systemDefault())
-                .toLocalDateTime();
-        deviceCredentialService.reissue(payload.getUserId(), payload.getDeviceId(),
-                newToken.getRefreshToken(), newExpiredAt);
+                .toLocalDateTime());
 
         return newToken;
     }
@@ -85,16 +94,4 @@ public class AuthService implements AuthUseCase {
     public void signOut(SignOutRequest request) {
         deviceCredentialService.logout(request.getUserId(), request.getDeviceId());
     }
-
-    private Token issue(IssueRequest request) {
-        TokenPayload payload = TokenPayload.builder()
-            .userId(request.getUserId())
-            .deviceId(request.getDeviceId())
-            .issuedAt(new Date())
-            .build();
-
-        System.out.println("AuthService.issue() payload.getExpiredAt(): " + payload.getExpiredAt());
-        return jwtProvider.createToken(payload);
-    }
 }
-
