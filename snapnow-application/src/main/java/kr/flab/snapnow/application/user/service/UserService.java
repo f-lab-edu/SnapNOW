d22f9bca6da7df1f918016c51af9df68a3fb14f8
김혜주 <sueelly@naver.com>
2025-01-07 11:29:18 +0900

    refactor(UserService): Refactor signup method to apply changed things
    Related to #8

--- snapnow-application/src/main/java/kr/flab/snapnow/application/user/service/UserService.java
+++ snapnow-application/src/main/java/kr/flab/snapnow/application/user/service/UserService.java
@@ -3,16 +3,17 @@ package kr.flab.snapnow.application.user.service;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.stereotype.Service;
-
+import org.springframework.transaction.annotation.Transactional;
 import kr.flab.snapnow.core.exception.BadRequestException;
 import kr.flab.snapnow.core.exception.ForbiddenException;
 import kr.flab.snapnow.domain.auth.Token;
 import kr.flab.snapnow.domain.auth.exception.UserNotMatchPasswordException;
 import kr.flab.snapnow.domain.user.enums.account.AuthProvider;
-import kr.flab.snapnow.domain.user.model.User;
 import kr.flab.snapnow.domain.user.model.userAccount.credential.Email;
 import kr.flab.snapnow.domain.user.model.userAccount.credential.UserCredential;
+import kr.flab.snapnow.domain.user.model.userProfile.UserProfile;
 import kr.flab.snapnow.domain.user.model.userDevice.Device;
+import kr.flab.snapnow.application.user.usecase.dto.UserCreateDto;
 import kr.flab.snapnow.application.user.usecase.DeleteIdUseCase;
 import kr.flab.snapnow.application.user.usecase.SignUpUseCase;
 import kr.flab.snapnow.application.user.output.UserOutputPort;
@@ -20,27 +21,34 @@ import kr.flab.snapnow.application.auth.service.AuthService;
 import kr.flab.snapnow.application.auth.service.CredentialService;
 import kr.flab.snapnow.application.email.VerificationType;
 import kr.flab.snapnow.application.email.service.EmailService;
+import kr.flab.snapnow.application.auth.usecase.dto.IssueRequest;
 
 @Service
 @RequiredArgsConstructor
 public class UserService implements SignUpUseCase, DeleteIdUseCase {
 
-    private final AuthService authService;
+    private final UserDeviceService userDeviceService;
     private final CredentialService credentialService;
+    private final UserProfileService userProfileService;
+    private final UserInfoService userInfoService;
+    private final UserSettingService userSettingService;
+    private final AuthService authService;
     private final EmailService emailService;
     private final UserOutputPort userOutputPort;
 
-    public Token signUp(User user) {
-        Email email = user.getAccount().getCredential().getEmail();
-        Device device = user.getUserDevice().getDevices().get(0);
+    public Token signUp(UserCreateDto userCreateDto) {
+        Email email = userCreateDto.getCredential().getEmail();
 
         if (!emailService.isSuccess(
-                email, VerificationType.SIGNUP)) {
+            email, VerificationType.SIGNUP)) {
             throw new ForbiddenException("Email verification is needed before signing up");
         }
 
-        userOutputPort.insert(user);
-        return authService.issue(email, device.getDeviceId());
+        Long userId = createUser(userCreateDto);
+        return authService.issue(IssueRequest.builder()
+            .userId(userId)
+            .deviceId(userCreateDto.getDevice().getDeviceId())
+            .build());
     }
 
     public void deleteEmailUser(Long userId, String password, String deleteReason) {
@@ -70,8 +78,23 @@ public class UserService implements SignUpUseCase, DeleteIdUseCase {
         delete(userId, deleteReason);
     }
 
+    @Transactional
+    private Long createUser(UserCreateDto userCreateDto) {
+        Long userId = userOutputPort.insert();
+        Device device = userCreateDto.getDevice();
+        UserCredential userCredential = userCreateDto.getCredential();
+        UserProfile userProfile = userCreateDto.getProfile();
+
+        credentialService.insert(userCredential);
+        userProfileService.insert(userProfile);
+        userDeviceService.insert(userId, device);
+        userInfoService.insert(userId);
+        userSettingService.insert(userId);
+
+        return userId;
+    }
+
     private void delete(Long userId, String deleteReason) {
         userOutputPort.delete(userId, deleteReason);
     }
 }
-
