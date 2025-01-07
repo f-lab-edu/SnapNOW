0fc5d4d7d9f0ebc466044791f2f108bb4f633c03
김혜주 <sueelly@naver.com>
2025-01-07 11:04:52 +0900

    feat(UserService): Create User Credential/Divce/Info/Profile/Setting service and implements insert/delete method
    Using when creating user
    Related to #4

--- snapnow-application/src/main/java/kr/flab/snapnow/application/auth/service/CredentialService.java
+++ snapnow-application/src/main/java/kr/flab/snapnow/application/auth/service/CredentialService.java
@@ -3,6 +3,8 @@ package kr.flab.snapnow.application.auth.service;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import kr.flab.snapnow.application.auth.output.CredentialOutputPort;
 import kr.flab.snapnow.application.auth.usecase.CredentialUseCase;
 import kr.flab.snapnow.domain.user.model.userAccount.credential.Email;
@@ -10,9 +12,15 @@ import kr.flab.snapnow.domain.user.model.userAccount.credential.UserCredential;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class CredentialService implements CredentialUseCase {
 
     private final CredentialOutputPort credentialOutputPort;
+
+    @Transactional
+    public void insert(UserCredential userCredential) {
+        credentialOutputPort.insert(userCredential);
+    }
 
     public UserCredential getCredential(Long userId) {
         return credentialOutputPort.get(userId);
---
+++ snapnow-application/src/main/java/kr/flab/snapnow/application/user/service/UserDeviceService.java
@@ -0,0 +1,43 @@
+package kr.flab.snapnow.application.user.service;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import kr.flab.snapnow.application.user.output.UserDeviceOutputPort;
+import kr.flab.snapnow.domain.user.model.userDevice.Device;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class UserDeviceService {
+
+    private final DeviceService deviceService;
+    private final UserDeviceOutputPort userDeviceOutputPort;
+
+    @Transactional
+    public void insert(Long userId, Device device) {
+        if (count(device.getDeviceId()) == 0) {
+            deviceService.insert(device);
+        }
+
+        userDeviceOutputPort.insert(userId, device.getDeviceId());
+    }
+
+    public int count(Long userId) {
+        return userDeviceOutputPort.count(userId);
+    }
+
+    public int count(String deviceId) {
+        return userDeviceOutputPort.count(deviceId);
+    }
+
+    @Transactional
+    public void delete(Long userId, String deviceId) {
+        userDeviceOutputPort.delete(userId, deviceId);
+
+        if (count(deviceId) == 1) {
+            deviceService.delete(deviceId);
+        }
+    }
+}
